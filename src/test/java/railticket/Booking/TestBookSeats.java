680266c680849f066713965b61b650e189c81d1e
Mohamedfarooq S <farooqmohamed21@gmail.com>
2020-02-06 18:03:43 +0530

    second update

--- src/test/java/railticket/Booking/TestBookSeats.java
+++ src/test/java/railticket/Booking/TestBookSeats.java
@@ -1,13 +1,12 @@
 
 package railticket.Booking;
 
-import citipe.servicelayer.UserService;
-
 import java.time.LocalDate;
 
+import railticket.booking.Bookingimplements;
+import railticket.client.WalletAPI;
 import railticket.dao.BookingDAO;
 import railticket.dao.Logger;
-import railticket.booking.Bookingimplements;
 
 public class TestBookSeats {
 
@@ -19,13 +18,13 @@ public class TestBookSeats {
 			LocalDate travelDate = LocalDate.parse("2020-04-21");
 			int amt = obj.bookSeats(32636, 1025, "chennai", "Madurai", 6, travelDate);
 			
-			UserService userService = new UserService();
 			long mobileNumber = 9999999999L;
 			long originator = 6789012340L;
 			int pinNumber = 1234;
 			float amount = amt;
 			try {
-				boolean status = userService.walletTransfer(mobileNumber, originator, pinNumber, amount);
+
+				boolean status = WalletAPI.pay(mobileNumber, originator, pinNumber, amount);
 				System.out.println(status);
 				if(status==true) {
 					Logger.getInstance().info("SUCCESSFULLY BOOKED");
@@ -41,5 +40,7 @@ public class TestBookSeats {
 
 		}
 	}
+
+	
 
 }
