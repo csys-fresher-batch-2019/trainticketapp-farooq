5a0ef38fbd9158759ef2dd92e444d07d4597aed3
Mohamedfarooq S <farooqmohamed21@gmail.com>
2020-02-17 09:53:41 +0530

    updated

--- src/test/java/railticket/registration/TestchangePassword.java
+++ src/test/java/railticket/registration/TestchangePassword.java
@@ -7,7 +7,7 @@ public class TestchangePassword {
 
 	public static void main(String[] args)throws Exception {
 		RegistrationImplementation obj = new RegistrationImplementation();
-		obj.changePassword(1040,"pass112");
+		//obj.changePassword(1040,"pass112");
 System.out.println("SUCCESSFULLY CHANGED");
 	}
 
